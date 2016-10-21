32dafe5b98a973070625735475c8ffc6f2a84e60
OPISTO\ville-petteri.sassi <vpsassi@gmail.com>
2016-10-21 14:00:09 +0300

    CharacterMovement (WASD) done

--- RobotChild/Assets/Scripts/CharacterMovement.cs
+++ RobotChild/Assets/Scripts/CharacterMovement.cs
@@ -6,26 +6,13 @@ public class CharacterMovement : MonoBehaviour {
 	public float moveSpeed;
 
 
-	// Use this for initialization
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-	
-		if (Input.GetKey(KeyCode.W)) {
-			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-		}
-		if (Input.GetKey(KeyCode.S)) {
-			transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
-		}
-		if (Input.GetKey(KeyCode.A)) {
-			transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-		}
-		if (Input.GetKey(KeyCode.D)) {
-			transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-		}
+		transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * moveSpeed * Time.deltaTime);
 
 	}
 }
