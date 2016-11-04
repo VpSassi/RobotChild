1913c3871b221b568227e7ceb181d3495d73e55c
Tuomas Eero <eerotuo@gmail.com>
2016-11-04 10:23:04 +0200

    Test cannibal now tries to detect the player and moves towards the player if succesful

--- RobotChild/Assets/Scripts/EnemyBehavior.cs
+++ RobotChild/Assets/Scripts/EnemyBehavior.cs
@@ -3,39 +3,71 @@ using System.Collections;
 
 public class EnemyBehavior : MonoBehaviour {
 
-	public Transform[] points;
-	private int destPoint = 0;
-	Vector3 direction;
+    public float patrolSpeed;
+    public float closeEnoughToWP = 0.5f;
+    public Transform[] points;
+    public LayerMask layerMask;
+
+    private int destPoint = 0;
 	float remainingDistance;
-	public float patrolSpeed;
-	public float closeEnoughToWP = 0.5f;
-	NavMeshAgent navAgent;
+    GameObject player;
+    Vector3 direction;
+    Vector3 playerDirection;
+    NavMeshAgent navAgent;
+
+
+
+
+
 
 	// TODO: also support turning around at last WP?
 
 	void Awake() {
 		navAgent = GetComponent<NavMeshAgent>();
 		destPoint = points.Length - 1;
-		nextWp();		
+		NextWp();		
 	}
 
-	void nextWp() {
-		if (points.Length == 0)
-			return;
-
-		destPoint = (destPoint + 1) % points.Length;
-		print(destPoint);
-		navAgent.SetDestination(points[destPoint].position);
-	}
+    void Start() {
+        player = GameObject.Find("robotChild");
+        layerMask = ~layerMask;                                                                     //We want our raycasts to ignore selected layers
+    }
 
 	void Update() {
-		direction = points[destPoint].position - transform.position;
+        playerDirection = player.transform.position - transform.position;
+        direction = points[destPoint].position - transform.position;
 		remainingDistance = direction.magnitude;
 
-		if (remainingDistance < closeEnoughToWP) {
-			nextWp();
+        //Detection behavior
+        if (IsPlayerDetected()) {
+            navAgent.SetDestination(player.transform.position);
+        }
+
+        //Patrol logic
+        if (remainingDistance < closeEnoughToWP) {
+			NextWp();
 		}
 
 		// transform.position += direction.normalized * patrolSpeed * Time.deltaTime;
 	}
+
+
+
+
+
+    void NextWp() {
+        if (points.Length == 0)
+            return;
+
+        destPoint = (destPoint + 1) % points.Length;
+        navAgent.SetDestination(points[destPoint].position);
+    }
+
+    bool IsPlayerDetected() {
+        if (!Physics.Raycast(transform.position, playerDirection, playerDirection.magnitude, layerMask)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
