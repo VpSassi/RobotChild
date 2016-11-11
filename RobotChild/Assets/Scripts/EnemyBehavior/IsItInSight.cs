4ccc9a3516537a19a0967d4fd74ebccad6480c9c
Tuomas Eero <eerotuo@gmail.com>
2016-11-11 14:48:57 +0200

    Deleted MoveToLastPos script, added a new script LookAround

---
+++ RobotChild/Assets/Scripts/EnemyBehavior/IsItInSight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsItInSight : MonoBehaviour {
+
+    GameObject player;
+    PlayerAbilities pa;
+
+
+
+
+
+    void Start() {
+        player = GameObject.Find("robotChild");
+        pa = player.GetComponent<PlayerAbilities>();
+    }
+
+
+
+
+
+    public bool IsPlayerInSight(GameObject observer, LayerMask rayMask) {
+        Vector3 playerDirection = player.transform.position - observer.transform.position;
+
+        if (!Physics.Raycast(transform.position, playerDirection, playerDirection.magnitude, rayMask) && playerDirection.magnitude < pa.lightValue) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
