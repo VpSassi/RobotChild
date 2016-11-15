1049a31e95f31383e76482e66b2baa6b7e581ae5
Miksander <mikko1aaltonen@gmail.com>
2016-11-15 13:12:00 +0200

    sound tsting

---
+++ RobotChild/Assets/PlayAudio.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum CAMP_AUDIO
+{
+    INVALID             =       0,
+    THROWING_SOUND      =       1,
+}
+
+public class PlayAudio : MonoBehaviour {
+    public AudioSource throwingSound;
+
+    public void PlaySoundByName(CAMP_AUDIO name)
+    {
+        switch (name)
+        {
+            case CAMP_AUDIO.THROWING_SOUND:
+                Play(throwingSound);
+                break;
+
+            default:
+                print("INVALID");
+                break;
+        }
+    }
+
+    public void Play(AudioSource source)
+    {
+        if(source)
+        {
+            source.Play();
+        }
+    }
+}
