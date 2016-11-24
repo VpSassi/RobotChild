e4a96a3bccc14b60f891a1f7e77051e2c0933cd2
Tuomas Eero <eerotuo@gmail.com>
2016-11-24 09:10:10 +0200

    Main Menu skeleton created

---
+++ RobotChild/Assets/MainMenu/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoaByIndex(int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
