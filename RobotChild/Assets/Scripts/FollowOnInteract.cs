accbdc7fe86e9a91ebb1ad06cbacf19ee66da2eb
Tuomas Eero <eerotuo@gmail.com>
2016-11-28 19:15:00 +0200

    Clones now follow the RobotChild in a line

---
+++ RobotChild/Assets/Scripts/FollowOnInteract.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowOnInteract : MonoBehaviour {
+
+    GameObject player;
+    NavMeshAgent navAgent;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("robotChild");
+        navAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        navAgent.SetDestination(player.transform.position);
+	}
+}
