ed6c2a3ea50dd73a1925e28d1c68857a8814da31
Tuomas Eero <eerotuo@gmail.com>
2016-11-28 13:28:12 +0200

    RobotChild's tag changed from "Player" to "RobotChild"

---
+++ RobotChild/Assets/Scripts/EnemyBehavior/AcquireTarget.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AcquireTarget : MonoBehaviour {
+
+    public List<GameObject> robotChildren;
+
+	//public GameObject ClosestTarget() {
+
+ //   }
+}
