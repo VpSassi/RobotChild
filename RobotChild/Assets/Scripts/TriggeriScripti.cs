99db6f7751c41320470e67b179b3528e78c111e6
Miksander <mikko1aaltonen@gmail.com>
2016-11-28 12:28:06 +0200

    audio

--- RobotChild/Assets/Scripts/TriggeriScripti.cs
+++ RobotChild/Assets/Scripts/TriggeriScripti.cs
@@ -9,18 +9,19 @@ public class TriggeriScripti : MonoBehaviour
     public AudioMixerSnapshot mix2;
     public AudioMixerSnapshot mix3;
     public float transitiontime;
-
+    EnemyBehavior eb; 
     // Use this for initialization
     void Start()
     {
+        eb = GameObject.Find("Cannibal").GetComponent<EnemyBehavior>();
         Fabric.EventManager.Instance.PostEvent("BasicMusic"); // kaikki träkit ruoeavat soimaan startissa
     }
-   // void OnTriggerEnter(Collider c) 
-
+    void Update()
     {
-    
+        if (eb.isChasing == true) {
         mix1.TransitionTo(transitiontime);
         print("triggasi");
+        }
    
         }
 
