a359f28669118e39b3180416f5a0e301581e8e0f
Miksander <mikko1aaltonen@gmail.com>
2016-11-28 10:33:18 +0200

    mixersnapshot trigger imported

---
+++ RobotChild/Assets/Scripts/TriggeriScripti.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class TriggeriScripti : MonoBehaviour
+{
+
+    public AudioMixerSnapshot mix1;
+    public AudioMixerSnapshot mix2;
+    public AudioMixerSnapshot mix3;
+    public float transitiontime;
+
+    // Use this for initialization
+    void Start()
+    {
+        Fabric.EventManager.Instance.PostEvent("play music"); // kaikki träkit ruoeavat soimaan startissa
+    }
+    void OnTriggerEnter(Collider c) 
+    {
+    
+        mix1.TransitionTo(transitiontime);
+        print("triggasi");
+   
+        }
+
+    }
+
