dd9f20dd61157eff466c8f6199bdf04e7dcf2cb2
OPISTO\ville-petteri.sassi <vpsassi@gmail.com>
2016-11-29 14:15:33 +0200

    pressure plate mechanics ready

---
+++ RobotChild/Assets/Scripts/pressurePlate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pressurePlate : MonoBehaviour {
+
+	public Animator pPAnim;
+	public Animator doorAnim;
+	public bool isPressed;
+
+	void Update() {
+
+	if (isPressed == true){
+			pPAnim.SetBool("isPressed", true);
+			doorAnim.SetBool("Open", true);
+			//print("click");
+		}
+	}
+
+	void OnTriggerEnter(Collider plate) {
+
+		if (plate.tag == ("Player")) {
+			isPressed = true;
+		}
+		else {
+			isPressed = false;
+			pPAnim.SetBool("isPressed", false);
+			doorAnim.SetBool("Open", false);
+		}
+	}
+
+
+
+}
