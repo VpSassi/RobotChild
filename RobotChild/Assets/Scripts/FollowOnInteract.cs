6cba6cc30512584034bae163f876bb9d7966989e
Tuomas Eero <eerotuo@gmail.com>
2016-11-29 09:30:12 +0200

    FIxed clone following bug

--- RobotChild/Assets/Scripts/FollowOnInteract.cs
+++ RobotChild/Assets/Scripts/FollowOnInteract.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FollowOnInteract : MonoBehaviour {
 
+    public float stoppingDistance;
+
     GameObject player;
     NavMeshAgent navAgent;
 
@@ -14,6 +16,13 @@ public class FollowOnInteract : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        navAgent.SetDestination(player.transform.position);
-	}
+        if ((navAgent.transform.position - player.transform.position).magnitude > stoppingDistance) {
+            navAgent.Resume();
+            navAgent.SetDestination(player.transform.position);
+        }
+        if ((navAgent.transform.position - player.transform.position).magnitude < stoppingDistance) {
+            navAgent.Stop();
+        }
+        print((navAgent.transform.position - player.transform.position).magnitude);
+    }
 }
