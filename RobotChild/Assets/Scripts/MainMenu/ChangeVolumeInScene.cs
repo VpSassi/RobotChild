f7a74c24ce1386db93d3a57077b3c5651591a4ea
Chidar_K_ <eerotuo@gmail.com>
2016-12-06 14:31:58 +0200

    Master Volume works now

---
+++ RobotChild/Assets/Scripts/MainMenu/ChangeVolumeInScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeVolumeInScene : MonoBehaviour {
+
+    public float masterVolume = 1f;
+
+
+
+
+
+    void Update() {
+        AudioListener.volume = masterVolume;
+    }
+
+
+
+
+
+    public void ChangeVolume(float volume) {
+        masterVolume = volume;
+    }
+
+}
